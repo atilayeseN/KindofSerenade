a45467316f3ce37265b56a42adc8af4a66506a5e
atilayeseN <38512694+atilayeseN@users.noreply.github.com>
2021-04-29 01:11:24 +0300

    First Commit

---
+++ game.js
@@ -0,0 +1,28 @@
+var counter=0;
+clicks();
+function clicks(){
+
+	var body=document.body,butt,i,battın;
+	var randtop=String(Math.floor(Math.random() * 500) + 1);
+	var randbottom=String(Math.floor(Math.random() * 1000) + 1);
+    var randleft=String(Math.floor(Math.random() * 1000) + 1);
+    var randright=String(Math.floor(Math.random() * 1000) + 1);
+    butt=document.createElement('button');
+    butt.style.position="absolute";
+    butt.style.top=randtop+"px";
+    butt.style.left	=randright+"px";
+    body.appendChild(butt);
+    butt.innerHTML='Buraya tıkla';
+    butt.addEventListener('click', e=>{
+      butt.parentNode.removeChild(butt);
+      clicks();
+      counter++;     
+    });
+    if(counter===5){
+    butt.parentNode.removeChild(butt); 
+    body.style.backgroundImage = "url('giphy.gif')";
+    var text=document.getElementById("love");
+    text.style.display="inherit";
+    }
+
+  }
